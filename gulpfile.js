000ac31654e6549cf2cb550af997622f20092cef
Munna Ahmed <munnaahmed2025@gmail.com>
2024-03-07 17:42:52 +0600

    try cleanCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 const { src, dest, watch, series } = require("gulp");
+// const cleanCSS = require("gulp-clean-css");
 const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src("./shinobi/**/*.scss")
-    .pipe(sass().on("error", sass.logError))
-    .pipe(dest("./css"));
+  return (
+    src("./shinobi/**/*.scss")
+      .pipe(sass().on("error", sass.logError))
+      // .pipe(cleanCSS())
+      .pipe(dest("./css"))
+  );
 }
 
 function buildWatch() {
