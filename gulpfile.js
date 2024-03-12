cf60681b8e010169dc6a3f89d20bb77692f1af00
Munna Ahmed <munnaahmed2025@gmail.com>
2024-03-12 20:31:57 +0600

    purgecss with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
 const { src, dest, watch, series } = require("gulp");
-const cleanCSS = require("gulp-clean-css");
+const cleanCSS = require("gulp-clean-css"); // minify css
+const purgecss = require("gulp-purgecss");
 const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return (
-    src("./shinobi/**/*.scss")
-      .pipe(sass().on("error", sass.logError))
-      // .pipe(cleanCSS())
-      .pipe(dest("./css"))
-  );
+  return src("./shinobi/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(purgecss({ content: ["*.html"] }))
+    .pipe(cleanCSS())
+    .pipe(dest("./css"));
 }
 
 function buildWatch() {
-  watch(["./shinobi/**/*.scss"], buildStyles);
+  watch(["./shinobi/**/*.scss", "*.html"], buildStyles);
 }
 
 exports.default = series(buildStyles, buildWatch);
